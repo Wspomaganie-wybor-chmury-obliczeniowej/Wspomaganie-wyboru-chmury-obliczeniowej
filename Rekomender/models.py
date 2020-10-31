d61c3238545b9ec7cfc7d19a762bf628d679f2ea
Wasiewski <oskar.wasiewski@wp.pl>
2020-10-31 10:09:21 +0100

    Update models.py

--- Rekomender/models.py
+++ Rekomender/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from jsonfield import JSONField
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)  
+
+    def __str__(self):
+        return self.choice_text  
+
+class Cloud(models.Model):
+    provider_name = models.CharField(max_length=50) # wszystko z tabelki
+    def __str__(self):
+        return self.Cloud_text
+    #list of services ? 
+# class Service(models.Model):
+#     name =  models.CharField(max_length=100)
+#     weight = models.IntegerField(default=1)
+class Client(models.Model):
+    client_name = models.CharField(max_length=50)
+    client_experience = models.IntegerField(default=0)
+    def __str__(self):
+        return self.Client_text
+# class VirtualUser(models.Model):
+#     name = models.CharField(max_length=50)
+class QuestionChoices(models.Model):
+    info = JSONField(null=True, blank=True)
+
+
+#przykładowy json, zamiast tworzyć CSV zrobić jsona, łatwiejszy w imporcie
+#  "questions":[ 
+#  {
+#   "question_text": "W jaki sposób chciałbyś się skontaktować z serwisantem/ pomocnikiem do konfiguracji?"
+#   "date_added": "random",
+#   "Choices": [
+#     {
+#       "text": "Telefonicznie",   
+#     },
+#     {
+#       "type": "Email",   
+#     }
+#   ],
+#   "flag_if_multiple": 0
+# }
+# ],
+  #check this out !!!!! https://django-import-data.readthedocs.io/en/latest/
